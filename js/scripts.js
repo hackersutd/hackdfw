525ceee8499cf0bb450e3370110a38d748c8d0b1
Bear Cott <bear@bearcott.com>
2014-11-23 02:42:17 -0600

    major design update

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,14 @@ $(document).ready(function() {
       window.location.hash = target;
     });
   });
+  var time=0;
+  setInterval(function() {
+    wh = $(window).height();
+    st = $(document).scrollTop();
+    if (st <= wh) {
+      $('#headerContent').css('transform','translate3d(' + -(st) + 'px,0,0)');
+      $('#headerContent').css('opacity', (1-st/wh*5).toFixed(2));
+    }
+    //$('#metropolis').css('transform','translate3d(0,' + Math.sin(time++)*20 + 'px,0)');
+  }, 10)
 });
