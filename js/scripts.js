85b51518009d8a58b471747e6d03ad2272872f5a
Bear Cott <bear@bearcott.com>
2014-11-24 00:55:00 -0600

    responsiveness improved, animations added

--- js/scripts.js
+++ js/scripts.js
@@ -15,9 +15,16 @@ $(document).ready(function() {
   setInterval(function() {
     wh = $(window).height();
     st = $(document).scrollTop();
+    about = $('#about');
     if (st <= wh) {
-      $('#headerContent').css('transform','translate3d(' + -(st) + 'px,0,0)');
-      $('#headerContent').css('opacity', (1-st/wh*5).toFixed(2));
+      $('#headerContent').css('transform','translate3d(0,' + -(st/5) + 'px,0)');
+      $('#headerContent').css('opacity', (1-st/wh*2).toFixed(2));
+    }
+    if (st <= (about.scrollTop() + wh)) {
+      $('#about h1, #about p').css('opacity', ((st)/(about.offset().top)).toFixed(2));
+    }
+    if (st <= (about.offset().top + wh)) {
+      $('#headerbg').css('transform','translate3d(0,' + (50-(st/(about.offset().top+wh)*50)) + 'px,0)');
     }
     //$('#metropolis').css('transform','translate3d(0,' + Math.sin(time++)*20 + 'px,0)');
   }, 10)
